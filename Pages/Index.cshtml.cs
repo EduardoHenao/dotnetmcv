1e470d54e11400800d92134fcf07ad8dd5f5ba91
Eduardo Henao <eduardo.henao@outlook.com>
2024-08-15 19:14:22 -0500

    addind dependency injection for conf service and extract values from appsettings.json

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,15 +5,18 @@ namespace dotnet.Pages;
 
 public class IndexModel : PageModel
 {
+    private readonly IConfiguration _configuration;
     private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
     {
+        _configuration = configuration;
         _logger = logger;
     }
 
     public void OnGet()
     {
-
+        var who = _configuration["Position:name"];
+        if (who is not null) Console.WriteLine(who);
     }
 }
